85f27eeb16f08a3db7fdf39cfd8f7b103aea720a
guboc11 <guboc11@gmail.com>
2025-11-17 16:55:33 +0900

    remove provider

--- 4week/sui-typescript-sdk-hands-on/src/main.tsx
+++ 4week/sui-typescript-sdk-hands-on/src/main.tsx
@@ -3,24 +3,13 @@ import ReactDOM from "react-dom/client";
 import "@mysten/dapp-kit/dist/index.css";
 import "@radix-ui/themes/styles.css";
 
-import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Theme } from "@radix-ui/themes";
 import App from "./App.tsx";
-import { networkConfig } from "./networkConfig.ts";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Theme appearance="dark">
-      <QueryClientProvider client={queryClient}>
-        <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
-          <WalletProvider autoConnect>
-            <App />
-          </WalletProvider>
-        </SuiClientProvider>
-      </QueryClientProvider>
+      <App />
     </Theme>
   </React.StrictMode>,
 );
